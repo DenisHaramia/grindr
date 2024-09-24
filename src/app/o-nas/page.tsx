8fb8670bdf8d2a48a1450e507582fdb7f213eacb
Denis Haramia <denisko.haramia@gmail.com>
2024-09-24 12:57:22 +0200

    pages add

---
+++ src/app/o-nas/page.tsx
@@ -0,0 +1,15 @@
+//src/app/o-nas/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "oNas" };
+
+export default function oNas() {
+
+    return (
+        <Container>    
+            <Typography> oNas </Typography>
+        </Container>
+    );
+}
