dbb84114ecb195e9d8c861eefc2a6c7b493136cb
Denis Haramia <denisko.haramia@gmail.com>
2024-10-08 11:25:33 +0200

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,31 @@
 //src/components/NavBar.tsx
 
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+"use client";
 
-export const metadata = { title: "NavBar" };
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export default function NavBar() {
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-    return (
-        <Container>    
-            <Typography> Nav Bar </Typography>
-        </Container>
-    );
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
 }
