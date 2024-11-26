5069a7c776251a11ee7f61d8ae1d048024484ad9
DenisHaramia <denisko.haramia@gmail.com>
2024-11-26 12:11:20 +0100

    register add

--- src/app/auth/prihlasenie/page.tsx
+++ src/app/auth/prihlasenie/page.tsx
@@ -1,15 +1,25 @@
 //src/app/auth/prihlasenie/page.tsx
 
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import LoginComponent from "@/components/LoginComponent"
 
 export const metadata = { title: "Login" };
 
 export default function Login() {
 
     return (
-        <Container>    
-            <Typography> Login </Typography>
+        <Container   
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '90vh',
+        textAlign: 'center',
+      }}
+    >
+      <LoginComponent />
         </Container>
     );
 }
